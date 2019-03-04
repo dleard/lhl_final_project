4d117cf0262673b04faf5eb4e40b4c1d4803b2e2
Dylan Leard <dleard@uvic.ca>
2019-03-04 18:49:47 +0000

    add rough left-slide-pane functionality

--- src/client/App.jsx
+++ src/client/App.jsx
@@ -4,7 +4,10 @@ import SlidingPane from 'react-sliding-pane';
 import 'react-sliding-pane/dist/react-sliding-pane.css';
 
 export default class App extends Component {
-  state = { username: null };
+  state = {
+    username: null,
+    isPaneOpenLeft: true
+  };
 
   componentDidMount() {
     fetch('/api/getUsername')
@@ -13,9 +16,18 @@ export default class App extends Component {
   }
 
   render() {
-    const { username } = this.state;
     return (
       <div>
+        <button onClick={() => this.setState({ isPaneOpenLeft: true })}>Click me to open right pane!</button>
+        <SlidingPane
+                closeIcon={<div>Some div containing custom close icon.</div>}
+                isOpen={ this.state.isPaneOpenLeft }
+                title='Hey, it is optional pane title.  I can be React component too.'
+                from='left'
+                width='50%'
+                onRequestClose={ () => this.setState({ isPaneOpenLeft: false }) }>
+                <div>And I am pane content on left.</div>
+            </SlidingPane>
         <img style={{width: "100vw", height: "100vh"}} src={ReactImage} alt="react" />
       </div>
     );

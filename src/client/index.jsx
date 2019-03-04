f4b33fd3d49e2b7a976ac03c18f37ff777fc5bf8
Dylan Leard <dleard@uvic.ca>
2019-03-04 19:16:02 +0000

    import fontawesome libs to index.jsx and add necessary icons to library

--- src/client/index.jsx
+++ src/client/index.jsx
@@ -3,5 +3,10 @@ require("./styles/app.scss");
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faAngleDoubleLeft, faAngleDoubleRight } from '@fortawesome/free-solid-svg-icons'
+
+library.add(faAngleDoubleLeft, faAngleDoubleRight)
 
 ReactDOM.render(<App />, document.getElementById('root'));

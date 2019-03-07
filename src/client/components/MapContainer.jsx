fed1330da88f6acfc4a7f8a18b6c6ad3cc4be41e
Dylan Leard <dleard@uvic.ca>
2019-03-07 22:22:09 +0000

    add last raw metar to info window & a button to add to the trip planner (not functional yet

--- src/client/components/MapContainer.jsx
+++ src/client/components/MapContainer.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import AirportMarker from './AirportMarker.jsx';
 import {Map, InfoWindow, Marker, GoogleApiWrapper} from 'google-maps-react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 
 const uuidv4 = require('uuid/v4');
@@ -28,10 +28,30 @@ export class MapContainer extends Component {
     });
   }
 
+  renderMetars = () => {
+    const metars = [];
+    
+    if (this.props.metars !== null && this.state.activeMarker.station !== undefined) {
+      console.log(this.state.activeMarker.station);
+    console.log(this.props.metars[0])
+      this.props.metars.forEach((metar) => {
+        if (this.state.activeMarker.station[0] === metar.station_id[0]) { metars.push(metar)}
+      })
+      return  <li key = {metars[0].raw_text[0]} className="list-group-item">{metars[0].raw_text[0]}</li>
+      // return metars.map((metar) => {
+      //   return (
+      //     <li key = {metar.raw_text[0]} className="list-group-item">{metar.raw_text[0]}</li>
+      //   )
+      // })
+        
+    }
+  }
+
   render() {
     let markers;
     if (this.props.metars != null) {
       console.log('metar loaded!');
+      console.log(this.props.metars);
       markers = this.props.metars.map(marker => (
         <Marker options={{icon: '/public/airport.png', label: `${marker.station_id}`}} onClick={this.onMarkerClick} key={uuidv4()} station={marker.station_id} position={{ lat: `${marker.latitude[0]}`, lng: `${marker.longitude[0]}` }} /> 
       ));
@@ -44,8 +64,12 @@ export class MapContainer extends Component {
             marker = { this.state.activeMarker }
             visible = { this.state.showingInfoWindow }
           >
-            <h1>HELLO</h1>
-            
+            <ul className="list-group">
+            <li style={{textAlign: "center"}}className="list-group-item active">Raw Metars</li>
+            {this.renderMetars()}
+          </ul>
+          <button className="btn btn-primary" onClick={() => console.log('hi')} type="button"><FontAwesomeIcon icon="plus-circle"></FontAwesomeIcon>Add to Planner</button>
+
           </InfoWindow>
           
         </Map>
